04f8a611d7aa55cbf5581ebda1e5e03e5c40e45e
Gill-Singh-A <amansg22@iitk.ac.in>
2024-04-09 00:41:38 +0530

    Added Index Route

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #! /usr/bin/env python3
 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+host = "127.0.0.1"
+port = 7080
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
-    pass
+    app.run(host=host, port=port, debug=True)
